8809bb98da32372c13eb652ff5ed94a9818edd9e
Bob van der Valk <info@mawoo.nl>
2015-12-31 15:29:35 +0100

    CodeIgniter install

---
+++ application/libraries/ComposerLoader.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Bob
+ * Date: 31-12-2015
+ * Time: 15:25
+ */
+class ComposerLoader
+{
+    public function __construct() {
+        include './vendor/autoload.php';
+    }
+}
