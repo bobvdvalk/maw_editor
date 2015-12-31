de83909ff62a92d434b5f1f69d037bca9aede133
Bob van der Valk <info@mawoo.nl>
2015-12-31 02:00:12 +0100

    set up config page

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bob
+ * Date: 31-12-2015
+ * Time: 01:56
+ */
+class Config
+{
+    public $url_path = "http://localhost/";
+
+    public $path = "C:/xampp/htdocs/maw-editor/";
+
+    public $project_path = "C:/xampp/htdocs/";
+}
