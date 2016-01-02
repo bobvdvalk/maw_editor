8d4d016588d3226ca41bc9d970f722858d772cd5
Bob van der Valk <info@mawoo.nl>
2016-01-02 16:38:50 +0100

    Demo code
    Makes no sense I know

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -5,7 +5,21 @@
                 <h1>Migration monitor</h1>
             </div>
             <p>This is where you can schedule/edit/add operations of your migration.</p>
-            <pre id="editor"></pre>
+            <pre id="editor">var value = 13;
+
+function test(blabla) {
+    console.log("Open function test");
+
+    switch(blabla) {
+        case 13:
+            return "Het getal is 13";
+        case 12:
+            return "Het getal is 12";
+    }
+}
+
+console.log(test(value));
+</pre>
         </div>
     </div>
 </div>
