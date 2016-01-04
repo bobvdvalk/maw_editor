5613fe4c6e1c18e22f6f298c8c7a44b86f315d0e
Bob van der Valk <info@mawoo.nl>
2016-01-04 19:53:49 +0100

    Refactoring demo code

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -5,21 +5,6 @@
                 <h1>Migration monitor</h1>
             </div>
             <p>This is where you can schedule/edit/add operations of your migration.</p>
-            <pre id="editor">var value = 13;
-
-function test(blabla) {
-    console.log("Open function test");
-
-    switch(blabla) {
-        case 13:
-            return "Het getal is 13";
-        case 12:
-            return "Het getal is 12";
-    }
-}
-
-console.log(test(value));
-</pre>
         </div>
     </div>
 </div>
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -3,12 +3,5 @@
 
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-<script type="text/javascript">
-    <?php $this->load->helper('url'); ?>
-    ace.require("ace/ext/language_tools");
-    var editor = ace.edit("editor");
-    editor.session.setMode("ace/mode/javascript");
-    editor.setTheme("ace/theme/tomorrow");
-</script>
 </body>
 </html>
